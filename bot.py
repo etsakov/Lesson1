a51cb795c26eae9afa5a8b7d4aad0770b5514cf4
etsakov <eitsakov@yahoo.com>
2017-05-27 18:19:56 +0300

    bot first version

---
+++ bot.py
@@ -0,0 +1,31 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import logging
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def main():
+    updater = Updater("397661166:AAE9wy8s1Laf9C_V6cdhUNPWh2L912C5Uj8")
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    updater.start_polling()
+    updater.idle()
+
+def greet_user(bot, update):
+    print('Вызван /start')
+    bot.sendMessage(update.message.chat_id, text='Давай общаться!')
+
+def show_error(bot, update, error):
+    print(error)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+main()
